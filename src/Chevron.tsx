48220fc60c6582fe5d9451bfa7a23442d48b8210
JeonJaewon <wksk04515@naver.com>
2021-08-28 22:39:31 +0900

    Add transition to Chevron

--- src/Chevron.tsx
+++ src/Chevron.tsx
@@ -1,20 +1,24 @@
 import React, { ComponentPropsWithoutRef, forwardRef, Ref } from "react"
+import styled from 'styled-components';
 
 interface ChevronProps extends ComponentPropsWithoutRef<'svg'> {
   size: string;
-  weight?: number;
 }
+
+const ChevronWrapper = styled.svg<ChevronProps>`
+  transition: all 0.4s ease;  
+`;
 
 export const Chevron = forwardRef((
   { size, ...props }: ChevronProps,
   ref: Ref<SVGSVGElement>
   ) => {
     return (
-      <svg width={size} height={size} viewBox="0 0 14 24" ref={ref} fill='#F1F3F4' {...props}>
+      <ChevronWrapper width={size} height={size} viewBox="0 0 14 24" ref={ref} fill='#F1F3F4' {...props}>
         <g>
           <path d='M13.0607 13.0607C13.6464 12.4749 13.6464 11.5251 13.0607 10.9393L3.51472 1.3934C2.92893 0.807612 1.97918 0.807612 1.3934 1.3934C0.807612 1.97918 0.807612 2.92893 1.3934 3.51472L9.87868 12L1.3934 20.4853C0.807612 21.0711 0.807612 22.0208 1.3934 22.6066C1.97918 23.1924 2.92893 23.1924 3.51472 22.6066L13.0607 13.0607ZM10 13.5H12V10.5H10V13.5Z' />
         </g>
-      </svg>
+      </ChevronWrapper >
     );
   }
 );
