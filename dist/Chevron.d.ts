512c51e84f1f5dddcfe7cb77d18b2168ec99f81f
JeonJaewon <wksk04515@naver.com>
2021-08-30 15:17:39 +0900

    Modify Chevron props

--- dist/Chevron.d.ts
+++ dist/Chevron.d.ts
@@ -1,7 +1,6 @@
 import React, { ComponentPropsWithoutRef } from "react";
 interface ChevronProps extends ComponentPropsWithoutRef<'svg'> {
     size: string;
-    weight?: number;
 }
 export declare const Chevron: React.ForwardRefExoticComponent<ChevronProps & React.RefAttributes<SVGSVGElement>>;
 export {};
--- src/Chevron.tsx
+++ src/Chevron.tsx
@@ -5,8 +5,8 @@ interface ChevronProps extends ComponentPropsWithoutRef<'svg'> {
   size: string;
 }
 
-const ChevronWrapper = styled.svg<ChevronProps>`
-  transition: all 0.4s ease;  
+const ChevronWrapper = styled.svg`
+  transition: all 0.4s ease;
 `;
 
 export const Chevron = forwardRef((
@@ -18,7 +18,7 @@ export const Chevron = forwardRef((
         <g>
           <path d='M13.0607 13.0607C13.6464 12.4749 13.6464 11.5251 13.0607 10.9393L3.51472 1.3934C2.92893 0.807612 1.97918 0.807612 1.3934 1.3934C0.807612 1.97918 0.807612 2.92893 1.3934 3.51472L9.87868 12L1.3934 20.4853C0.807612 21.0711 0.807612 22.0208 1.3934 22.6066C1.97918 23.1924 2.92893 23.1924 3.51472 22.6066L13.0607 13.0607ZM10 13.5H12V10.5H10V13.5Z' />
         </g>
-      </ChevronWrapper >
+      </ChevronWrapper>
     );
   }
 );
