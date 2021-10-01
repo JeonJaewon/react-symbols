d2750a9d3b0fe658b9509fd98df9fe1bb4a7c953
JeonJaewon <wksk04515@naver.com>
2021-10-01 18:09:07 +0900

    Add color prop to Chevron

--- dist/Chevron.d.ts
+++ dist/Chevron.d.ts
@@ -1,6 +1,7 @@
 import React, { ComponentPropsWithoutRef } from "react";
 interface ChevronProps extends ComponentPropsWithoutRef<'svg'> {
     size: string;
+    color?: string;
 }
 export declare const Chevron: React.ForwardRefExoticComponent<ChevronProps & React.RefAttributes<SVGSVGElement>>;
 export {};
--- dist/Chevron.js
+++ dist/Chevron.js
@@ -53,8 +53,8 @@ var react_1 = __importStar(require("react"));
 var styled_components_1 = __importDefault(require("styled-components"));
 var ChevronWrapper = styled_components_1.default.svg(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  transition: all 0.4s ease;\n"], ["\n  transition: all 0.4s ease;\n"])));
 exports.Chevron = react_1.forwardRef(function (_a, ref) {
-    var size = _a.size, props = __rest(_a, ["size"]);
-    return (react_1.default.createElement(ChevronWrapper, __assign({ width: size, height: size, viewBox: "0 0 14 24", ref: ref, fill: '#F1F3F4' }, props),
+    var size = _a.size, _b = _a.color, color = _b === void 0 ? '#F1F3F4' : _b, props = __rest(_a, ["size", "color"]);
+    return (react_1.default.createElement(ChevronWrapper, __assign({ width: size, height: size, viewBox: "0 0 14 24", ref: ref, fill: color }, props),
         react_1.default.createElement("g", null,
             react_1.default.createElement("path", { d: 'M13.0607 13.0607C13.6464 12.4749 13.6464 11.5251 13.0607 10.9393L3.51472 1.3934C2.92893 0.807612 1.97918 0.807612 1.3934 1.3934C0.807612 1.97918 0.807612 2.92893 1.3934 3.51472L9.87868 12L1.3934 20.4853C0.807612 21.0711 0.807612 22.0208 1.3934 22.6066C1.97918 23.1924 2.92893 23.1924 3.51472 22.6066L13.0607 13.0607ZM10 13.5H12V10.5H10V13.5Z' }))));
 });
